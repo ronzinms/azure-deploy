4d8bea01c27c23f773186dbb577326978ed91878
ronzinms <112196514+ronzinms@users.noreply.github.com>
2022-09-09 16:51:06 -0400

    Update create-workflow-pr.sh

--- .github/script/create-workflow-pr.sh
+++ .github/script/create-workflow-pr.sh
@@ -10,10 +10,10 @@ git config user.email github-actions@github.com
 if [ "$1" = "--pull-first" ]
 then
     echo "Merging main into $PR_BRANCH"
-    git checkout $PR_BRANCH
-    git pull origin main --no-rebase -X theirs --no-edit
-    git push origin $PR_BRANCH
+    git checkout ronzinms:$PR_BRANCH
+    git pull origin ronzinms:main --no-rebase -X theirs --no-edit
+    git push origin ronzinms:$PR_BRANCH
 fi
 
 echo "Create pull request for $PR_BRANCH into main"
-gh pr create --base main --head $PR_BRANCH --title "$PR_TITLE" --body ""
+gh pr create --base ronzinms:main --head ronzinms:$PR_BRANCH --title "$PR_TITLE" --body ""
